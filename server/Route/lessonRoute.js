48c8b1db220f71496859517dadfb60ebcecfb21f
Sahar <bellem75@yahoo.com>
2024-03-26 11:10:38 -0700

    create json file and make the router to make get request

---
+++ server/Route/lessonRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router(); 
+const fs = require('fs');
+
+// Read data from JSON file
+router.get('/', (req,res) => {
+	const readData = fs.readFileSync('./data/lesson.json');
+	const readDataJson = JSON.parse(readData);
+	res.status(200).send(readDataJson);
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -8,11 +8,16 @@ require('dotenv').config({path: './.env'});
 // loading variable from .env
 const PORT = process.env.PORT;
 
+const lessonRoute = require('./Route/lessonRoute');
+
 // Middleware setup
 
 app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/lesson', lessonRoute);
 
 app.listen(PORT, ()=> {
 	console.log(`Server is running on Port: ${PORT}`);
